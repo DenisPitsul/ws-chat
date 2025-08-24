c4b8505145950402267db168c459cd8b1471378f
Denis Pitsul <denia.pitsul@gmail.com>
2025-08-24 12:24:04 +0300

    use server link on client

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,27 +1,28 @@
-import axios from 'axios';
-import queryString from 'query-string';
-import CONSTANTS from '../constants';
+import axios from "axios";
+import queryString from "query-string";
+import CONSTANTS from "../constants";
 
 const axiosOptions = {
-  baseURL: 'http://localhost:5001/api',
+  baseURL: "https://ws-chat-mcql.onrender.com/api",
 };
 
 const apiInstance = axios.create(axiosOptions);
 
-export const getMessages = limit => apiInstance.get(`/messages?limit=${limit}`);
+export const getMessages = (limit) =>
+  apiInstance.get(`/messages?limit=${limit}`);
 
-export const register = user => apiInstance.post('/auth/registration', user);
+export const register = (user) => apiInstance.post("/auth/registration", user);
 
-export const login = user => apiInstance.post('/auth/login', user);
+export const login = (user) => apiInstance.post("/auth/login", user);
 
-export const getUser = token =>
-  apiInstance.get('/user', {
+export const getUser = (token) =>
+  apiInstance.get("/user", {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
 
-export const getGroups = payload => {
+export const getGroups = (payload) => {
   const query = queryString.stringify({
     groupName: payload.groupName,
     page: payload.page,
